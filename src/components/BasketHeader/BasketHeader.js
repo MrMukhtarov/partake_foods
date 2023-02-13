0846ecf63e54c85cd6b99102d0c3def1dde3d8b4
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-02-13 23:17:22 +0400

    header_basket_Menu

--- src/components/BasketHeader/BasketHeader.js
+++ src/components/BasketHeader/BasketHeader.js
@@ -15,12 +15,12 @@ const BasketHeader = () => {
         <div className="blocker" onClick={hidePopup}></div>
       <div className="all">
         <div className="top d-flex align-items-center">
-          <div className="left col-lg-10 align-items-center">
+          <div className="left col-lg-10 align-items-center col-10">
             <h3>Your Cart</h3>
             <span className="mb-1">(0)</span>
           </div>
           <div className="right col-lg-2">
-            <i onClick={basketClose} class="fa-solid fa-xmark" title="close"></i>
+            <i onClick={basketClose} className="fa-solid fa-xmark" title="close"></i>
           </div>
         </div>
         <div className="bottom">
